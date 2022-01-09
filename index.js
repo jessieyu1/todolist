b200bbaef17e706e6ff406d1c8a316c81c3e02c1
Jessie Yu <61171021+jessieyu1@users.noreply.github.com>
2022-01-09 21:05:09 +0800

    add get by id and post

--- index.js
+++ index.js
@@ -1,35 +1,66 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 let data = [
-    {
-        "description": "task No.1",
-        "done": false
-    },
-    {
-        "description": "task No.2",
-        "done": false
-    },
-    {
-        "description": "task No.3",
-        "done": false
-    },
-    {
-        "description": "task No.4",
-        "done": false
-    },
-    {
-        "description": "task No.5",
-        "done": false
-    }, 
-  ]
-app.get('/tasks', (req, res) => {
-    res.json(data)    
-}
-)
+  { id: 1, description: "task No.1", done: false },
+  {
+    id: 2,
+    description: "task No.2",
+    done: false,
+  },
+  {
+    id: 3,
+    description: "task No.3",
+    done: false,
+  },
+  {
+    id: 4,
+    description: "task No.4",
+    done: false,
+  },
+  {
+    id: 5,
+    description: "task No.5",
+    done: false,
+  },
+];
 
-const PORT = 8000
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const generateId = () => {
+    const maxId = data.length > 0 ? data[data.length - 1].id : 0;
+    return maxId + 1;
+  };
+
+app.get("/tasks", (req, res) => {
+  res.json(data);
+});
+
+app.get("/tasks/:id", (req, res) => {
+    const id = Number(req.params.id)
+    const task = data.find(task => task.id === id)
+    if (task) {
+        res.json(task)
+    }
+    res.status(404).end()
+})
+
+app.post("/tasks", (req, res) => {
+  const body = req.body;
+  if (!body.description) {
+    return res.status(400).json({
+      error: "You must provide a description",
+    });
+  }
+  const task = {
+    id: generateId(),
+    description: body.description,
+    done: body.done || false,
+  };
+  data.push(task);
+  res.json(task);
+});
+
+const PORT = 8000;
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
